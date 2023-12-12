0d66d51460582e00ef4c892963d25873d5c12543
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-12-12 10:32:58 -0600

    Working on Carousel Fullscreen

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const burguerMenu = document.querySelector('.burguer-menu');
 const mobileDropMenu = document.querySelector('.mobile-menu');
 const headInMenu = document.querySelector('.BuscarInmueble');
 const dropHeadInMenu = document.querySelector('.drop-menu-desk');
+const carousel = document.getElementById('carouselExample');
 
 burguerMenu.addEventListener('click', toggleMenu)
 headInMenu.addEventListener('click', toggleMenu)
@@ -13,3 +14,17 @@ function toggleMenu(event) {
     dropHeadInMenu.classList.toggle('inactive')
   }
 };
+
+// full screen function
+
+function openFullscreen( openCarousel ) {
+  openCarousel = carousel;
+
+  if (openCarousel.requestFullscreen) {
+    openCarousel.requestFullscreen();
+  } else if (openCarousel.webkitRequestFullscreen) { /* Safari */
+    openCarousel.webkitRequestFullscreen();
+  } else if (openCarousel.msRequestFullscreen) { /* IE11 */
+    openCarousel.msRequestFullscreen();
+  }
+}
